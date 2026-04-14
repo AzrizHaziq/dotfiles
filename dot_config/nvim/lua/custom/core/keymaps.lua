9edc2725e691a3c9fc9ceab37e0b31fdacb41421
azrizhaziq <azrizhaziq@gmail.com>
2026-04-14 13:06:48 +0800

    update

--- dot_config/nvim/lua/custom/core/keymaps.lua
+++ dot_config/nvim/lua/custom/core/keymaps.lua
@@ -55,16 +55,17 @@ end
 vim.keymap.set('n', '<leader>ca', copy_absolute_path, { desc = 'Copy absolute path with line:col' })
 vim.keymap.set('n', '<leader>cr', copy_relative_path, { desc = 'Copy relative path with line:col' })
 
--- Toggle relative/absolute line numbers
-vim.keymap.set('n', '<leader>tn', function()
-  if vim.o.relativenumber then
-    vim.o.relativenumber = false
-    vim.o.number = true
-  else
-    vim.o.relativenumber = true
-    vim.o.number = true
-  end
-end, { desc = 'Toggle relative/absolute number' })
+-- -- Toggle relative/absolute line numbers
+-- vim.keymap.set('n', '<leader>tn', function()
+--   if vim.o.relativenumber then
+--     vim.o.relativenumber = false
+--   else
+--     vim.o.relativenumber = true
+--   end
+-- end, { desc = 'Toggle relative/absolute number' })
+
+vim.keymap.set('n', '<leader>tw', '<cmd>set wrap!<CR>', { desc = 'Toggle [W]ord wrap' })
+vim.keymap.set('n', '<leader>tn', '<cmd>set relativenumber!<CR>', { desc = 'Toggle [N]umber or relative number' })
 
 -- vim.keymap.set('n', '<leader>rr', function()
 --   for name, _ in pairs(package.loaded) do
@@ -78,7 +79,6 @@ end, { desc = 'Toggle relative/absolute number' })
 -- end, { desc = 'Reload Config' })
 
 vim.keymap.set('n', '<leader>wa', '<cmd>wa<CR>', { desc = '[W]rite [A]ll files' })
-vim.keymap.set('n', '<leader>ww', '<cmd>set wrap!<CR>', { desc = 'Toggle [W]ord [W]rap' })
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
