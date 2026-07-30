42982ee512a4293714ba78a6fa74a01f037eb25a
azrizhaziq <azrizhaziq@gmail.com>
2026-07-30 10:57:21 +0800

    update command

--- dot_config/nvim/lua/core/commands.lua
+++ dot_config/nvim/lua/core/commands.lua
@@ -1,1 +1,9 @@
-require {}
+vim.api.nvim_create_user_command(
+  'SetColumnWidth',
+  function(opts)
+    -- Default to 80 if no argument is given, otherwise use the argument
+    local width = opts.args ~= '' and opts.args or '80'
+    vim.opt.colorcolumn = width
+  end,
+  { nargs = '?' } -- Allows 0 or 1 argument
+)
