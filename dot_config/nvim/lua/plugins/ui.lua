e3ae2a64ea161fffc882ac6859a5310c21d65ad3
azrizhaziq <azrizhaziq@gmail.com>
2026-08-10 10:37:15 +0800

    update sesh, removed fzf-tmux

--- dot_config/nvim/lua/plugins/ui.lua
+++ dot_config/nvim/lua/plugins/ui.lua
@@ -13,6 +13,23 @@ return {
   },
 
   {
+    '2kabhishek/nerdy.nvim',
+    dependencies = {
+      'folke/snacks.nvim',
+    },
+    cmd = 'Nerdy',
+    opts = {
+      max_recents = 30, -- Configure recent icons limit
+      copy_to_clipboard = true, -- Copy glyph to clipboard instead of inserting
+      copy_register = '+', -- Register to use for copying (if `copy_to_clipboard` is true)
+    },
+    keys = {
+      { '<leader>in', '<cmd>Nerdy list<CR>', desc = 'Browse nerd icons' },
+      { '<leader>iN', '<cmd>Nerdy recents<CR>', desc = 'Browse recent nerd icons' },
+    },
+  },
+
+  {
     'kevinhwang91/nvim-ufo',
     dependencies = 'kevinhwang91/promise-async',
     event = 'VeryLazy',
